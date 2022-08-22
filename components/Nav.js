c542283336269b896684185008aafcce55a64365
mdraihan34988 <swammia908@gmail.com>
2022-08-22 22:58:44 +0600

    adaa

--- components/Nav.js
+++ components/Nav.js
@@ -36,77 +36,28 @@ const Nav = () => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
   return (
-    <nav
-      className="
-          flex flex-wrap
-          items-center
-          justify-between
-          w-full
-          py-4
-          md:py-0
-          px-4
-          text-lg text-gray-700
-          bg-white
-        "
-    >
-      <div>
-        <Link href="/" passHref>
-          <a>
-            {/* eslint-disable @next/next/no-img-element */}
-            <img
-              className="m-3"
-              src="/logo.png"
-              width={50}
-              height={20}
-              alt="Strapi Logo"
-            />
-          </a>
-        </Link>
-      </div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        id="menu-button"
-        className="h-6 w-6 cursor-pointer md:hidden block"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M4 6h16M4 12h16M4 18h16"
-        />
-      </svg>
-
-      <div
-        className="hidden w-full md:flex md:items-center md:w-auto"
-        id="menu"
-      >
-        <ul
-          className="
-              pt-4
-              text-base text-gray-700
-              md:flex
-              md:justify-between 
-              md:pt-0 space-x-2"
-        >
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+    <div class="container flex flex-wrap justify-between items-center mx-auto">
+      <a href={"/"} class="flex items-center">
+          <img src="/logo.png" width="50px" height="20px"class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Strapi</span>
+      </a>
+      <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <span class="sr-only">Menu</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+      </button>
+      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
-              <a href={"/"} className="md:p-2 py-2 block hover:text-purple-400">Home</a>
-
+            <a href={"/"} class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
           </li>
           <li>
-            
-              <a className="md:p-2 py-2 block hover:text-purple-400"href={"/restaurents"}>
-                Restaurents
-              </a>
-
+            <a href={"/restaurents"} class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Restaurents</a>
           </li>
-         
-          
         </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
   );
 };
 
